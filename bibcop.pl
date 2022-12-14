81b50e33faa11796948c89c8c4cd28bb08b6cf03
Yegor Bugayenko <yegor256@gmail.com>
2022-12-14 08:42:35 +0300

    #12 better help msg

--- bibcop.pl
+++ bibcop.pl
@@ -483,14 +483,15 @@ sub warning {
 }
 
 if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
-  debug("Bibcop is a Style Checker of .bib Files\n" .
+  debug("Bibcop is a Style Checker of BibTeX Files\n\n" .
     "Usage:\n" .
     "  bibcop [<options>] <.bib file path>\n\n" .
     "Options:\n" .
     "  -v, --version Print the current version of the tool and exit\n" .
     "  -?, --help    Print this help screen\n" .
     "      --fix     Fix the errors and print a new version of the .bib file to the console\n" .
-    "      --latex   Report errors in LaTeX format using \\PackageWarningNoLine command");
+    "      --latex   Report errors in LaTeX format using \\PackageWarningNoLine command\n\n" .
+    "If any issues, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
   debug('0.0.0');
 } else {
