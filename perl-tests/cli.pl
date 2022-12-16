7056db985691048a26c85822d71b7b2ad31880a9
Yegor Bugayenko <yegor256@gmail.com>
2022-12-16 10:06:16 +0300

    #18 +check

--- perl-tests/cli.pl
+++ perl-tests/cli.pl
@@ -30,6 +30,7 @@ assert_exec('-v', 0, qr/0\.0\.0/s);
 assert_exec('--help', 0, qr/--version /s);
 assert_exec('-?', 0, qr/--version /s);
 assert_exec('./test-files/test.bib', 0, qr/\QThe 'title' must be wrapped\E/s);
+assert_exec('--verbose ./test-files/test.bib', 0, qr/\QChecking knuth1974\E/s);
 assert_exec('--latex ./test-files/test.bib', 0, qr/\Q\PackageWarningNoLine{bibcop}{The 'title' must be wrapped\E/s);
 assert_exec('--fix ./test-files/test.bib', 0, qr/\Q{{The TeX Book}}\E/s);
 
