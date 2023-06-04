cd6f1217c28413f31f4cc4ffcccf6f2c243e8972
Yegor Bugayenko <yegor256@gmail.com>
2023-06-04 20:48:15 +0800

    #34 extra test

--- perl-tests/fixing-in-place.pl
+++ perl-tests/fixing-in-place.pl
@@ -30,18 +30,35 @@ my ($out, $temp) = tempfile();
 print $out '@article{knuth, author = {Knuth, Donald E}} @book{lamport, title={LaTeX}}';
 close($out);
 
-my $stdout = `perl ./bibcop.pl --in-place --fix '$temp' 2>&1`;
-if (not $stdout eq '') {
-  print $stdout;
+my $stdout1 = `perl ./bibcop.pl --in-place --fix '$temp' 2>&1`;
+if (not $stdout1 eq '') {
+  print $stdout1;
   print "This output was not expected!\n";
   exit 1;
 }
 
-open(my $in, '<', $temp);
-my $after; { local $/; $after = <$in>; }
+open(my $in1, '<', $temp);
+my $after1; { local $/; $after1 = <$in1>; }
+close($in1);
 
-if (index($after, 'Knuth, Donald E.') == 1) {
+if (index($after1, 'Knuth, Donald E.') == 1) {
   print "Didn't fix in place\n";
+  exit 1;
+}
+
+my $stdout2 = `perl ./bibcop.pl --in-place --fix '$temp' 2>&1`;
+if (not $stdout2 eq '') {
+  print $stdout2;
+  print "This output was not expected!\n";
+  exit 1;
+}
+
+open(my $in2, '<', $temp);
+my $after2; { local $/; $after2 = <$in2>; }
+close($in2);
+
+if (not $after2 eq $after1) {
+  print "Fixing again, why?\n";
   exit 1;
 }
 
