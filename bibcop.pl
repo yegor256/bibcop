a26937831104cd6846ac7f5f460ab7f79f825d4c
yegor256 <yegor256@gmail.com>
2023-09-29 02:54:43 +0000

    Deploying to gh-pages from @ yegor256/bibcop@deceda06ead36461c4b24b9d92a429515c3efb8e 🚀

--- bibcop.pl
+++ bibcop.pl
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# 2023-09-29 02.53.18
+# 2023-09-29 02.54.42
 package bibcop;
 
 use warnings;
@@ -861,7 +861,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --latex     Report errors in LaTeX format using \\PackageWarningNoLine command\n\n" .
     "If any issues, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('02.53.18 2023-09-29');
+  info('02.54.42 2023-09-29');
 } else {
   my ($file) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $file) {
