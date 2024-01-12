218b15f6fbd2d80b02fc6c1fb76bcdb73804bdb8
rultor <me@rultor.com>
2024-01-12 06:33:05 +0000

    version set to 0.0.17

--- bibcop.pl
+++ bibcop.pl
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# 0000-00-00 0.0.0
+# 2024-01-12 0.0.17
 package bibcop;
 
 use warnings;
@@ -917,7 +917,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --latex     Report errors in LaTeX format using \\PackageWarningNoLine command\n\n" .
     "If any issues, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('0.0.0 0000-00-00');
+  info('0.0.17 2024-01-12');
 } else {
   my ($file) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $file) {
