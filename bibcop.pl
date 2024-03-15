b2042c420397ac7a21de272a7e7adb3fcbe0351e
yegor256 <yegor256@gmail.com>
2024-03-15 14:19:38 +0000

    Deploying to gh-pages from @ yegor256/bibcop@7187184c3e3a4abc983b4fc9a3ee81133cfdef64 🚀

--- bibcop.pl
+++ bibcop.pl
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# 2024-03-15 14.18.47
+# 2024-03-15 14.19.38
 package bibcop;
 
 use warnings;
@@ -958,7 +958,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --latex     Report errors in LaTeX format using \\PackageWarningNoLine command\n\n" .
     "If any issues, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('14.18.47 2024-03-15');
+  info('14.19.38 2024-03-15');
 } else {
   my ($file) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $file) {
