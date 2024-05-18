9285bbafc4ba31f65fbb3160b7802b24efd1fd19
rultor <me@rultor.com>
2024-05-18 16:39:58 +0000

    Deploying to gh-pages from @ yegor256/bibcop@0e7a93b07a12beeaa29ccb67a378943d8eae23eb 🚀

--- bibcop.pl
+++ bibcop.pl
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# 2024-05-03 04.16.53
+# 2024-05-18 16.39.57
 package bibcop;
 
 use warnings;
@@ -1084,7 +1084,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --latex     Report errors in LaTeX format using \\PackageWarningNoLine command\n\n" .
     "If any issues, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('04.16.53 2024-05-03');
+  info('16.39.57 2024-05-18');
 } else {
   my ($file) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $file) {
