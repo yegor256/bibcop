00d4afab10a9861df717247cfbd2f10035005179
yegor256 <yegor256@gmail.com>
2025-05-04 17:13:25 +0000

    Deploying to gh-pages from @ yegor256/bibcop@f2f9a00bc4853cc141b3f72f8bf0451dfc1a505e 🚀

--- bibcop.pl
+++ bibcop.pl
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022-2025 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
-# 2025-05-04 17.06.51
+# 2025-05-04 17.13.24
 package bibcop;
 
 use warnings;
@@ -1083,7 +1083,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --latex     Report errors in LaTeX format using the \\PackageWarningNoLine command\n\n" .
     "If any issues, please, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('17.06.51 2025-05-04');
+  info('17.13.24 2025-05-04');
 } else {
   my ($file) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $file) {
