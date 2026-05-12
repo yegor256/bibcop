31aa81da8c50d977f788d7f9413dd7f41e06cf1b
rultor <me@rultor.com>
2026-05-12 17:25:16 +0000

    Deploying to gh-pages from @ yegor256/bibcop@442d84c14ed5c32677a87d4e2fa5f07e0882749f 🚀

--- bibcop.pl
+++ bibcop.pl
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
-# 0000-00-00 09.08.37
+# 0000-00-00 17.25.15
 package bibcop;
 
 use warnings;
@@ -1090,7 +1090,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --latex     Report errors in LaTeX format using the \\PackageWarningNoLine command\n\n" .
     "If any issues, please, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('09.08.37 0000-00-00');
+  info('17.25.15 0000-00-00');
 } else {
   my ($file) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $file) {
