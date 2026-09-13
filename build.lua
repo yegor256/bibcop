3b0e64d27a181ccc41d1bd2c3872458fd3072bd4
Rultor.com <gpg@rultor.com>
2026-09-13 06:19:34 +0000

    version set to 0.0.36

--- build.lua
+++ build.lua
@@ -20,7 +20,7 @@ checkruns = 2
 
 uploadconfig = {
   pkg = "bibcop",
-  version = "0.0.0 0000-00-00",
+  version = "0.0.36 2026-09-13",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
